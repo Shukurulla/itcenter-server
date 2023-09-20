8cd247edf800830ffd357caa8a980d152d712133
Shukurulla <shukurullatursinbayev978@gmail.com>
2023-09-20 14:05:45 +0500

    edit file

--- routers/mentor.js
+++ routers/mentor.js
@@ -7,24 +7,24 @@ router.get("/mentors", async (req, res) => {
   res.json({ data: mentors });
 });
 
-router.post("/add-mentor", async(req, res) => {
+router.post("/add-mentor", async (req, res) => {
   await Mentor.create(req.body.mentor);
-  const mentors = await Mentor.find()
-  res.json({data: mentors})
+  const mentors = await Mentor.find();
+  console.log(req.body);
+  res.json({ data: mentors });
 });
-
 
 router.post("/edit-mentor/:id", async (req, res) => {
   const id = req.params.id;
   await Mentor.findByIdAndUpdate(id, req.body.mentor);
   const mentors = await Mentor.find();
-  res.json({data:mentors});
+  res.json({ data: mentors });
 });
 router.delete("/delete-mentor/:id", async (req, res) => {
   const id = req.params.id;
   await Mentor.findByIdAndRemove(id);
   const mentors = await Mentor.find();
-  res.json({data:mentors});
+  res.json({ data: mentors });
 });
 
 module.exports = router;
